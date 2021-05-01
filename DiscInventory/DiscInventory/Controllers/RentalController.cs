4d9a6e8d89b37543d735a16c8babd0d319c4c53d
Patraic-Kelly <63427924+Patraic-Kelly@users.noreply.github.com>
2021-04-30 22:18:44 -0600

    Added Rental management page, Created Add and edit functionality including pre-filled fields.

---
+++ DiscInventory/DiscInventory/Controllers/RentalController.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DiscInventory.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DiscInventory.Controllers
+{
+    public class RentalController : Controller
+    {
+        private discInventoryPKContext context { get; set; }
+        public RentalController (discInventoryPKContext ctx)
+        {
+            context = ctx;
+        }
+        public IActionResult Index()
+        {
+            var rentals = context.Rentals.
+                Include(d => d.Disc).OrderBy(d => d.Disc.DiscName).
+                Include(b => b.Borrower).ToList();
+            return View(rentals);
+        }
+        [HttpGet]
+        public IActionResult Add()
+        {
+            ViewBag.Action = "Add";
+            ViewBag.Discs = context.Discs.OrderBy(d => d.DiscName).ToList();
+            ViewBag.Borrowers = context.Borrowers.OrderBy(l => l.LName).ToList();
+            Rental newrental = new Rental();
+            newrental.BorrowDate = DateTime.Now;
+            newrental.DueDate = newrental.BorrowDate.AddDays(30);
+            return View("Edit", newrental);
+        }
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            ViewBag.Action = "Edit";
+            ViewBag.Discs = context.Discs.OrderBy(d => d.DiscName).ToList();
+            ViewBag.Borrowers = context.Borrowers.OrderBy(l => l.LName).ToList();
+            var rental = context.Rentals.Find(id);
+            return View(rental);
+        }
+        [HttpPost]
+        public IActionResult Edit(Rental rental)
+        {
+            if (ModelState.IsValid)
+            {
+                if (rental.RentalId == 0)
+                {
+                    context.Rentals.Add(rental);
+                }
+                else
+                {
+                    context.Rentals.Update(rental);
+                }
+                    context.SaveChanges();
+                    return RedirectToAction("Index", "Rental");
+                
+            }
+            else
+            {
+                ViewBag.Action = (rental.RentalId == 0) ? "Add" : "Edit";
+                ViewBag.Discs = context.Discs.OrderBy(d => d.DiscName).ToList();
+                ViewBag.Borrowers = context.Borrowers.OrderBy(l => l.LName).ToList();
+                return View(rental);
+            }
+        }
+    }
+}
--- DiscInventory/DiscInventory/Models/Rental.cs
+++ DiscInventory/DiscInventory/Models/Rental.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using System.ComponentModel.DataAnnotations;
 #nullable disable
 
 namespace DiscInventory.Models
@@ -8,10 +8,14 @@ namespace DiscInventory.Models
     public partial class Rental
     {
         public int RentalId { get; set; }
+        [Required(ErrorMessage = "Please enter date of rental.")]
         public DateTime BorrowDate { get; set; }
+        [Required(ErrorMessage = "Please enter Due Date.")]
         public DateTime DueDate { get; set; }
         public DateTime? ReturnDate { get; set; }
+        [Required(ErrorMessage = "Please select borrower.")]
         public int BorrowerId { get; set; }
+        [Required(ErrorMessage = "Please select disc.")]
         public int DiscId { get; set; }
 
         public virtual Borrower Borrower { get; set; }
